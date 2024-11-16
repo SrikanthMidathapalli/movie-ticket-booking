74a688976f0e760acf058b3d413becea46797624
SrikanthMidathapalli <srikanth.midathapalli97@gmail.com>
2024-11-16 17:45:04 +0530

    Added movie-ticket-parent for shared configurations pom.xml file

---
+++ movie-ticket-parent/movie-ticket-parent/src/main/java/com/microservices/movie_ticket_parent/MovieTicketParentApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.movie_ticket_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieTicketParentApplication.class, args);
+	}
+
+}
---
+++ movie-ticket-parent/movie-ticket-parent/src/test/java/com/microservices/movie_ticket_parent/MovieTicketParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.movie_ticket_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
